8daa36cb857041430939cf366f544bac6916da0d
DMMCAMP310 <ec2-user@ip-172-31-41-167.ap-northeast-1.compute.internal>
2022-11-07 16:43:34 +0000

    [Add group]

--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,7 @@ Rails.application.routes.draw do
     get 'followers' => 'relationships#followers', as: 'followers'
   end
   resources :chats, only: [:show,:create]
-  resources :groups, only: [:index, :show, :edit, :new, :create]
+  resources :groups, only: [:index, :show, :edit, :new, :create, :update]
   get 'search' => 'searches#search'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
